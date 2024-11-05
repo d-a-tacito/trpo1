50e5b2868fd7ffc51d52a33d12c67cd5d2d097b8
dantonov <dananto02@gmail.com>
2024-11-05 20:35:25 +0300

    Added cycle from person1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,10 @@
         {
             Console.WriteLine("Hello World from person2 and now from person1 and now from person3!");
             Console.WriteLine("New Hello World from person1");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Number of Hello World: {i}");
+            }
         }
     }
 }
