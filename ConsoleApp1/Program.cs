ec8a05d1c9c7aa965df5016f1e73082cba433aa8
dantonov <dananto02@gmail.com>
2024-11-05 21:33:21 +0300

    Used git apply from dev3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hello World from person2 and now from person1 and now from person3!");
             Console.WriteLine("New Hello World from person1");
+
+            Console.WriteLine("First change by person3");
+            Console.WriteLine("Second change by person3");
         }
     }
 }
